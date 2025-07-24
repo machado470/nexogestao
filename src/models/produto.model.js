0ab2238a87ecbef21fcf49f2888a4ccdcd6c002f
machado470 <williammachado5525@gmail.com>
2025-07-24 16:04:46 -0300

    Add produtos route structure

---
+++ src/models/produto.model.js
@@ -0,0 +1,9 @@
+class Produto {
+  constructor({ id, nome, descricao, preco }) {
+    this.id = id;
+    this.nome = nome;
+    this.descricao = descricao;
+    this.preco = preco;
+  }
+}
+module.exports = Produto;
