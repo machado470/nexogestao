8cb1048c06a97c9e0ae4bfb4f7f9e306721a20b4
William Machado <williammachado5525@gmail.com>
2025-07-25 00:39:45 -0300

    ajuste: afrouxa regras do eslint para facilitar desenvolvimento

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-HEAD
-    node: true,
     es2021: true,
+    node: true
   },
-  globals: {
-    process: 'readonly',
-    require: 'readonly',
-    module: 'readonly',
-    __dirname: 'readonly',
-    setImmediate: 'readonly',
-    Deno: 'readonly',
-  },
-
-    es2021: true,
-  },
-e29dd900d596d3914769d4e7096c21a4241dbf29
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended'
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: ['react'],
   rules: {
- HEAD
-    'no-undef': 'off',
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'no-empty': 'off',
-
-    // pode adicionar regras aqui depois
- e29dd900d596d3914769d4e7096c21a4241dbf29
-  },
+    // Suas regras personalizadas aqui
+  }
 };
