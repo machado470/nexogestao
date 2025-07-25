c93186e82123eeea8b7b10f426b0ffa349363eac
William Machado <williamachado5525@gmail.com>
2025-07-24 21:40:46 -0300

    mensagem forçada

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
-. "$(dirname "$0")/_/husky.sh"
+if [ -z "$husky_skip_init" ]; then
+  debug () {
+    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
+  }
+
+  readonly hook_name="$(basename "$0")"
+  debug "starting $hook_name..."
+
+  if [ "$HUSKY" = "0" ]; then
+    debug "HUSKY env variable is set to 0, skipping hook"
+    exit 0
+  fi
+
+  if [ -f ~/.huskyrc ]; then
+    debug "sourcing ~/.huskyrc"
+    . ~/.huskyrc
+  fi
+fi
