679f5a702fec513213a95016eb7701fd4b97fb28
William Machado <williamachado5525@gmail.com>
2025-07-25 00:57:52 -0300

    Remover husky.sh depreciado para versão atual

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,19 +1,9 @@
-#!/bin/sh
-if [ -z "$husky_skip_init" ]; then
-  debug () {
-    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
-  }
+echo "husky - DEPRECATED
 
-  readonly hook_name="$(basename "$0")"
-  debug "starting $hook_name..."
+Please remove the following two lines from $0:
 
-  if [ "$HUSKY" = "0" ]; then
-    debug "HUSKY env variable is set to 0, skipping hook"
-    exit 0
-  fi
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-  if [ -f ~/.huskyrc ]; then
-    debug "sourcing ~/.huskyrc"
-    . ~/.huskyrc
-  fi
-fi
+They WILL FAIL in v10.0.0
+"
