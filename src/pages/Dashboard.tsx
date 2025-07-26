33daed690aee33c4407c2ee5cb255dcf56725707
machado470 <williammachado5525@gmail.com>
2025-07-26 19:21:08 -0300

    Create dashboard page and update routes

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Dashboard() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl md:text-3xl font-semibold text-center">
+        Bem-vindo ao Painel NexoGestão
+      </h1>
+    </div>
+  );
+}
