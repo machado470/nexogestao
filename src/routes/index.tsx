f5d2e112eadcddc5e24efeb6d3e8b3188380f802
William Machado <williamachado5525@gmail.com>
2025-07-26 19:10:41 -0300

    Ajusta rotas da aplicação em src/routes/index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from '../features/auth/Login';
-import Produtos from '../features/produtos';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Auth from '../pages/Auth';
+import Painel from '../pages/Painel'; // ajuste conforme seu projeto
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/produtos" element={<Produtos />} />
+        <Route path="/" element={<Auth />} />
+        <Route path="/painel" element={<Painel />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
