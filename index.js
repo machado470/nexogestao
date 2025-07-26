329e68803b9e2ca3fd786ab601935267186c0eeb
William Machado <williamachado5525@gmail.com>
2025-07-26 14:06:31 -0300

    feat: adiciona rota GET padrão no index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import authRoutes from './routes/auth/auth.routes.js';
-
+const express = require('express');
 const app = express();
-app.use(express.json());
+const port = 3000;
 
-app.use('/auth', authRoutes);
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando!');
+});
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
