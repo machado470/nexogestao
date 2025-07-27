e1f36585bc59bcf5636b436d3ce34b7cfb732be2
William Machado <williamachado5525@gmail.com>
2025-07-26 22:52:13 -0300

    feat: adiciona estrutura inicial das páginas Auth e Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-
 export default function Dashboard() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-2xl md:text-3xl font-semibold text-center">
-        Bem-vindo ao Painel NexoGestão
-      </h1>
+    <div>
+      <h1>Bem-vindo ao Painel</h1>
     </div>
   );
 }
